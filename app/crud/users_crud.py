342140069d5a6ee22c3ce8b175b7c3ae2dbb8b49
matin <matiin.hn@gmail.com>
2026-05-10 20:31:35 +0330

    feat: Add create admin user function

--- app/crud/users_crud.py
+++ app/crud/users_crud.py
@@ -3,13 +3,17 @@ from typing import Optional
 from sqlalchemy.orm import Session
 
 from app.schemas.user import UserCreate
-from app.models.user import User
-from app.core.security import verify_password
+from app.models.user import User, UserRole
+from app.core.security import verify_password, get_password_hash
 
 
 def get_user_by_username(db: Session, username: str) -> Optional[User]:
     """Get user by username"""
     return db.query(User).filter(User.username == username).first()
+
+def get_user_by_id(db: Session, user_id: str) -> Optional[User]:
+    """Get user by username"""
+    return db.query(User).filter(User.id == user_id).first()
 
 def get_user_by_email(db: Session, email: str) -> Optional[User]:
     """Get user by email"""
@@ -46,3 +50,20 @@ def authenticate(db: Session, username: str, password: str):
         db.refresh(db_user)
     return db_user
 
+
+def create_admin_user(db: Session, email: str, username: str, password: str, creator_admin_id: int = None):
+    """Create a new admin user"""
+    hashed_password = get_password_hash(password)
+    
+    admin_user = User(
+        email=email,
+        username=username,
+        hashed_password=hashed_password,
+        role=UserRole.ADMIN,
+        created_by_admin_id=creator_admin_id
+    )
+    
+    db.add(admin_user)
+    db.commit()
+    db.refresh(admin_user)
+    return admin_user
